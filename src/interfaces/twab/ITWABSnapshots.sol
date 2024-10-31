01e268f8b8dda9d6de4d8978f356c73b8a435358
Taeguk Kwon <xornrbboy@gmail.com>
2024-10-31 14:20:31 +0700

    🌊 Update

--- src/interfaces/twab/ITWABSnapshots.sol
+++ src/interfaces/twab/ITWABSnapshots.sol
@@ -21,6 +21,8 @@ interface ITWABSnapshots is IERC5805 {
    */
   error ERC5805FutureLookup(uint256 timestamp, uint256 now_);
 
+  //=========== NOTE: Snapshot & Delegation ===========//
+
   /**
    * @notice Returns the total supply snapshot at the current time.
    * @return balance The current total supply.
@@ -78,4 +80,21 @@ interface ITWABSnapshots is IERC5805 {
    * @param delegatee The address to delegate to.
    */
   function delegateByManager(address account, address delegatee) external;
+
+  //=========== NOTE: TWAB ===========//
+
+  /**
+   * @notice Returns the Time-Weighted Average Balance (TWAB) for an account between two timepoints.
+   * @param account The address of the account.
+   * @param startsAt The timestamp at which to start the TWAB calculation.
+   * @param endsAt The timestamp at which to end the TWAB calculation.
+   */
+  function getTWABByTimestampRange(address account, uint48 startsAt, uint48 endsAt) external view returns (uint256);
+
+  /**
+   * @notice Returns the Time-Weighted Average Balance (TWAB) for the total supply between two timepoints.
+   * @param startsAt The timestamp at which to start the TWAB calculation.
+   * @param endsAt The timestamp at which to end the TWAB calculation.
+   */
+  function getTotalTWABByTimestampRange(uint48 startsAt, uint48 endsAt) external view returns (uint256);
 }
